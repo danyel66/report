f366455dfb091eaa89c4b2039b1149e4e122c9d8
danyel66 <danielonyeka247@gmail.com>
2020-11-17 09:41:09 +0100

    first commit

---
+++ core/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+def index(request):
+    template_name = 'core/index.html'
+
+    context = {
+        
+    }
+    return render(request, template_name, context)
